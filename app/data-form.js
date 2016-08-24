fe294eec18185d13ba1072ff5467166c8f9fce02
lukostry <lukasz.ostrowski85@gmail.com>
2016-08-24 12:38:26 +0200

    Deprecated files removed

--- app/data-form.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var DataForm = (function () {
-    function DataForm() {
-        this.listOfMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    }
-    return DataForm;
-}());
-exports.DataForm = DataForm;
-//# sourceMappingURL=data-form.js.map
